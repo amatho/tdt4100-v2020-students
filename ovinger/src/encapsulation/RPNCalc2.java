d631ce6afd3aaa2253a59fbe799c525533cf2e18
Amandus Thorsrud <ama.thorsrud@gmail.com>
2020-01-30 13:42:29 +0100

    Fix code style of RPNCalc2

--- ovinger/src/encapsulation/RPNCalc2.java
+++ ovinger/src/encapsulation/RPNCalc2.java
@@ -5,126 +5,126 @@ import java.util.Stack;
 import java.util.stream.Stream;
 
 public class RPNCalc2 {
-	enum Operator {
-		Add, Sub, Mul, Div;
-	}
+  enum Operator {
+    ADD, SUB, MUL, DIV
+  }
 
-	enum TokenType {
-		Number, Operator;
-	}
+  enum TokenType {
+    NUMBER, OPERATOR
+  }
 
-	private interface Token {
-		public TokenType getTokenType();
-	}
+  private interface Token {
+    public TokenType getTokenType();
+  }
 
-	private class NumberToken implements Token {
-		private double value;
+  private class NumberToken implements Token {
+    private double value;
 
-		public NumberToken(double value) {
-			this.value = value;
-		}
+    public NumberToken(double value) {
+      this.value = value;
+    }
 
-		public double getValue() {
-			return value;
-		}
+    public double getValue() {
+      return value;
+    }
 
-		public TokenType getTokenType() {
-			return TokenType.Number;
-		}
-	}
+    public TokenType getTokenType() {
+      return TokenType.NUMBER;
+    }
+  }
 
-	private class OperatorToken implements Token {
-		private Operator operator;
-		private Token leftToken;
-		private Token rightToken;
+  private class OperatorToken implements Token {
+    private Operator operator;
+    private Token leftToken;
+    private Token rightToken;
 
-		public OperatorToken(Operator operator, Token left, Token right) {
-			this.operator = operator;
-			this.leftToken = left;
-			this.rightToken = right;
-		}
+    public OperatorToken(Operator operator, Token left, Token right) {
+      this.operator = operator;
+      this.leftToken = left;
+      this.rightToken = right;
+    }
 
-		public double evaluate() {
-			double left;
-			double right;
+    public double evaluate() {
+      double left;
+      double right;
 
-			if (leftToken.getTokenType() == TokenType.Operator) {
-				left = ((OperatorToken) leftToken).evaluate();
-			} else if (leftToken.getTokenType() == TokenType.Number) {
-				left = ((NumberToken) leftToken).getValue();
-			} else {
-				throw new UnsupportedOperationException("An unsupported token was encountered");
-			}
+      if (leftToken.getTokenType() == TokenType.OPERATOR) {
+        left = ((OperatorToken) leftToken).evaluate();
+      } else if (leftToken.getTokenType() == TokenType.NUMBER) {
+        left = ((NumberToken) leftToken).getValue();
+      } else {
+        throw new UnsupportedOperationException("An unsupported token was encountered");
+      }
 
-			if (rightToken.getTokenType() == TokenType.Operator) {
-				right = ((OperatorToken) rightToken).evaluate();
-			} else if (rightToken.getTokenType() == TokenType.Number) {
-				right = ((NumberToken) rightToken).getValue();
-			} else {
-				throw new UnsupportedOperationException("An unsupported token was encountered");
-			}
+      if (rightToken.getTokenType() == TokenType.OPERATOR) {
+        right = ((OperatorToken) rightToken).evaluate();
+      } else if (rightToken.getTokenType() == TokenType.NUMBER) {
+        right = ((NumberToken) rightToken).getValue();
+      } else {
+        throw new UnsupportedOperationException("An unsupported token was encountered");
+      }
 
-			switch (operator) {
-			case Add:
-				return left + right;
-			case Sub:
-				return left - right;
-			case Mul:
-				return left * right;
-			case Div:
-				return left / right;
-			default:
-				throw new UnsupportedOperationException(
-						String.format("No implementation for operator '%s'", operator.toString()));
-			}
-		}
+      switch (operator) {
+        case ADD:
+          return left + right;
+        case SUB:
+          return left - right;
+        case MUL:
+          return left * right;
+        case DIV:
+          return left / right;
+        default:
+          throw new UnsupportedOperationException(
+              String.format("No implementation for operator '%s'", operator.toString()));
+      }
+    }
 
-		public TokenType getTokenType() {
-			return TokenType.Operator;
-		}
-	}
+    public TokenType getTokenType() {
+      return TokenType.OPERATOR;
+    }
+  }
 
-	private static final String[] operatorStrings = {"+", "-", "*", "/"};
-	private OperatorToken rootToken;
+  private static final String[] OPERATOR_STRINGS = {"+", "-", "*", "/"};
+  private OperatorToken rootToken;
 
-	public RPNCalc2(String expr) {
-		var tokens = new Stack<String>();
-		tokens.addAll(Arrays.asList(expr.split(" ")));
-		
-		rootToken = (OperatorToken) parseTokens(tokens);
-	}
+  public RPNCalc2(String expr) {
+    var tokens = new Stack<String>();
+    tokens.addAll(Arrays.asList(expr.split(" ")));
 
-	public double calculate() throws IllegalStateException {
-		return rootToken.evaluate();
-	}
-	
-	private Token parseTokens(Stack<String> tokens) {
-		var toParse = tokens.pop();
-		
-		if (Stream.of(operatorStrings).anyMatch(toParse::equals)) {
-			var right = parseTokens(tokens);
-			var left = parseTokens(tokens);
-			
-			switch (toParse) {
-			case "+":
-				return new OperatorToken(Operator.Add, left, right);
-			case "-":
-				return new OperatorToken(Operator.Sub, left, right);
-			case "*":
-				return new OperatorToken(Operator.Mul, left, right);
-			case "/":
-				return new OperatorToken(Operator.Div, left, right);
-			default:
-				throw new UnsupportedOperationException();
-			}
-		} else {
-			return new NumberToken(Double.parseDouble(toParse));
-		}
-	}
+    rootToken = (OperatorToken) parseTokens(tokens);
+  }
 
-	public static void main(String[] args) {
-		var calc = new RPNCalc2("5 2 3 + * 4 - 7 /");
+  public double calculate() throws IllegalStateException {
+    return rootToken.evaluate();
+  }
 
-		System.out.println(calc.calculate());
-	}
+  private Token parseTokens(Stack<String> tokens) {
+    var toParse = tokens.pop();
+
+    if (Stream.of(OPERATOR_STRINGS).anyMatch(toParse::equals)) {
+      var right = parseTokens(tokens);
+      var left = parseTokens(tokens);
+
+      switch (toParse) {
+        case "+":
+          return new OperatorToken(Operator.ADD, left, right);
+        case "-":
+          return new OperatorToken(Operator.SUB, left, right);
+        case "*":
+          return new OperatorToken(Operator.MUL, left, right);
+        case "/":
+          return new OperatorToken(Operator.DIV, left, right);
+        default:
+          throw new UnsupportedOperationException();
+      }
+    } else {
+      return new NumberToken(Double.parseDouble(toParse));
+    }
+  }
+
+  public static void main(String[] args) {
+    var calc = new RPNCalc2("5 2 3 + * 4 - 7 /");
+
+    System.out.println(calc.calculate());
+  }
 }
